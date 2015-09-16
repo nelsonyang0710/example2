47130e40ae84389c07e0fd9d529e809e012ec733
nelson <nelson@talentcircles.com>
2015-09-15 17:55:56 -0700

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/userlist', function(req, res, next) {
+  var db = req.db;
+    var collection = db.get('userlist');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });});
+
+module.exports = router;
